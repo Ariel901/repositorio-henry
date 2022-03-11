bfe0b97142ac75ba43aaef7df91ace4f634b2109
Ariel901 <101021541+Ariel901@users.noreply.github.com>
2022-03-10 21:32:56 -0600

    Add files via upload

---
+++ curso js/prueba.js
@@ -0,0 +1,1 @@
+document.getElementById("cuadro").innerHTML = "analiza este codigo con el cual pudiste mostrar este mismo comentario";
---
+++ curso js/variab.js
@@ -0,0 +1,29 @@
+var x = 7;
+var pet = 4;
+var resta = x * pet;
+
+console.log(resta);
+
+var w = "ariel" +"  "+ "garcia" +"  "+ "martinez";
+
+console.log(w);
+
+
+var nom = "din din";
+var edad = "29";
+var datos = `yo soy ${nom} y tengo ${edad} años de edad.`;
+
+console.log(datos);
+
+
+const PI = 3.1416;
+console.log(PI);
+
+const Pa = 3.1417;
+console.log(Pa);
+
+
+
+
+
+
